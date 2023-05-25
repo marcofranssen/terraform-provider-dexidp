93fe842eb0ecef00279bf6e0f891c5cb028b255e
truongnht <truongnht@gmail.com>
2023-05-25 10:09:30 +0200

    Add Dex oauth2 client resource (#2)
    Co-authored-by: Marco Franssen <marco.franssen@gmail.com>

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -9,3 +9,12 @@ terraform {
 provider "dexidp" {
   host = "127.0.0.1:5557"
 }
+
+resource "dexidp_client" "my_oidc_client" {
+  name      = "Awesome Test Client"
+  client_id = "Awesome"
+  secret    = "T0pS3cr3t"
+  redirect_uris = [
+    "https://openidconnect.net/callback",
+  ]
+}
