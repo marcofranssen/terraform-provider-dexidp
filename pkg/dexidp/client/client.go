d143084a01a4a767a0981893364356f9c66fbb6d
Marco Franssen <marco.franssen@gmail.com>
2023-05-26 13:21:54 +0200

    Refactor deprecation notice

--- pkg/dexidp/client/client.go
+++ pkg/dexidp/client/client.go
@@ -5,11 +5,12 @@ import (
 
 	"github.com/dexidp/dex/api/v2"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 // New instantiates a new api.DexClient
 func New(host string) (api.DexClient, error) {
-	conn, err := grpc.Dial(host, grpc.WithInsecure())
+	conn, err := grpc.Dial(host, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, fmt.Errorf("dial: %w", err)
 	}
