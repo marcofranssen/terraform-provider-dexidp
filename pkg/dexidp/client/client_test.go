22e213e44a696ea00f0ac8aafe2b401652251093
Marco Franssen <marco.franssen@gmail.com>
2023-05-26 12:56:25 +0200

    Add unittest for creating a new Client

---
+++ pkg/dexidp/client/client_test.go
@@ -0,0 +1,39 @@
+package client_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/marcofranssen/terraform-provider-dexidp/pkg/dexidp/client"
+)
+
+func TestNew(t *testing.T) {
+	testCases := []struct {
+		name string
+		host string
+		err  error
+	}{
+		{
+			host: "",
+			err:  fmt.Errorf("dial: %w", errors.New("failed to build resolver: passthrough: received empty target in Build()")),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(tt *testing.T) {
+			assert := assert.New(tt)
+
+			c, err := client.New(tc.host)
+			if tc.err != nil {
+				assert.Nil(c)
+				assert.ErrorContains(err, tc.err.Error())
+			} else {
+				assert.NotNil(c)
+				assert.NoError(err)
+			}
+		})
+	}
+}
